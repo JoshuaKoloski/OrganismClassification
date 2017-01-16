8fae04f644989fc12e8c1fc6222064f75ec3bd97
JoshuaKoloski <joshuakoloski@gmail.com>
2017-01-15 23:22:41 -0500

    rudimentary organism

---
+++ Documents/Visual Studio 2015/Projects/OrganismClassification/OrganismClassification/Organism.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace OrganismClassification
+{
+    public abstract class Organism
+    {
+        bool alive = true;
+        public abstract int averageYearLifespan();
+        public abstract LinkedList<Organism> getDescendants();
+        public abstract LinkedList<Organism> getAncestors();
+        public abstract String behavior();
+
+    }
+}
