05e13cc32119a55a09a56961667bfa4295d5be1b
JoshuaKoloski <joshuakoloski@gmail.com>
2017-01-15 23:59:02 -0500

    basic animal kingdom and phylum

---
+++ Documents/Visual Studio 2015/Projects/OrganismClassification/OrganismClassification/Phylum.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrganismClassification
+{
+    abstract class Phylum : AnimalKingdom
+    {
+        public abstract String phylumName();
+        public abstract String nameMeaning();
+        public abstract String commonName();
+        public abstract String description();
+    }
+}
